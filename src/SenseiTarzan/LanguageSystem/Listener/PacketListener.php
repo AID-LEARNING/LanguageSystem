1e57864cf65181e86857fa7603d04e8a7c8e65d6
SenseiTarzan <gabrielcaptari0@gmail.com>
2026-04-05 12:26:07 +0200

    Refactor PacketListener: Optimize command description handling using reflection

--- src/SenseiTarzan/LanguageSystem/Listener/PacketListener.php
+++ src/SenseiTarzan/LanguageSystem/Listener/PacketListener.php
@@ -27,6 +27,9 @@ use pocketmine\event\EventPriority;
 use pocketmine\event\server\DataPacketSendEvent;
 use pocketmine\lang\Translatable;
 use pocketmine\network\mcpe\protocol\AvailableCommandsPacket;
+use pocketmine\network\mcpe\protocol\types\command\raw\CommandRawData;
+use ReflectionException;
+use ReflectionProperty;
 use SenseiTarzan\ExtraEvent\Class\EventAttribute;
 use SenseiTarzan\LanguageSystem\Component\LanguageManager;
 use WeakReference;
@@ -34,6 +37,8 @@ use WeakReference;
 class PacketListener
 {
 
+    /** @var array<string, ReflectionProperty> */
+    private static array $reflectionCache = [];
 	/**
 	 * @param WeakReference<LanguageManager> $langManager
 	 */
@@ -43,12 +48,16 @@ class PacketListener
 	{
 	}
 
-	#[EventAttribute(EventPriority::LOWEST)]
+    /**
+     * @throws ReflectionException
+     */
+    #[EventAttribute(EventPriority::LOWEST)]
 	public function onSendPacket(DataPacketSendEvent $event) : void
 	{
 		$listPacket = $event->getPackets();
 		$listNetworkSession = $event->getTargets();
 		$languageManager = $this->langManager->get();
+        $commandDataRawDescriptionProperty = self::getReflectionProperty(CommandRawData::class, "description");
 		foreach ($listPacket as $packet) {
 			if ($packet instanceof AvailableCommandsPacket) {
 				foreach ($packet->commandData as $_ => $commandDatum) {
@@ -60,11 +69,23 @@ class PacketListener
 						$descriptionTranslate = ($description instanceof Translatable ?
 							$languageManager->getTranslateModeNoSaveDefaultWithTranslatable($player, $description, default: $description) :
 							$languageManager->getTranslateModeNoSaveDefault($player, $description, default: $description));
-						$commandDatum->description = $descriptionTranslate;
+                        $commandDataRawDescriptionProperty->setValue($commandDatum, $descriptionTranslate);
 					}
 				}
 			}
 		}
 	}
 
+    /**
+     * @throws ReflectionException
+     */
+    private static function getReflectionProperty(object|string $object, string $property) : ReflectionProperty
+    {
+        $key = $object::class . "::" . $property;
+        if (!isset(self::$reflectionCache[$key])) {
+            $ref = new ReflectionProperty($object, $property);
+            self::$reflectionCache[$key] = $ref;
+        }
+        return self::$reflectionCache[$key];
+    }
 }
